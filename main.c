80e6e248bcc12bd118e7784da478bac15be9403b
CinquettiThomas <cinquettithomas@gmail.com>
2024-10-13 15:31:41 +0200

    prova con main

--- main.c
+++ main.c
@@ -1,11 +1,24 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include <wiringPi.h>
 
-int main()
-{
-   printf("Hello world!\n");
-   return 0;
-}
+#define LED_PIN 0  // Definisci il pin dove è collegato il LED (GPIO 17 corrisponde a wiringPi pin 0)
 
-//~
+int main(void) {
+    // Inizializza la libreria WiringPi
+    if (wiringPiSetup() == -1) {
+        return 1; // Esce in caso di errore di inizializzazione
+    }
+
+    // Imposta il pin come uscita
+    pinMode(LED_PIN, OUTPUT);
+
+    // Accende il LED
+    digitalWrite(LED_PIN, HIGH);
+
+    // Mantiene il LED acceso per 5 secondi
+    delay(5000);
+
+    // Spegne il LED
+    digitalWrite(LED_PIN, LOW);
+
+    return 0;
+}
